29a006c002faeaae422499444b70cec69851a445
Elise Erickson <elisece717@gmail.com>
2024-01-10 17:20:52 -0500

    fetches stocks

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+
+  const [stockList, setStockList] = useState()
+
+  useEffect( () => {
+    fetch("http://localhost:3001/stocks")
+    .then( res => res.json())
+    .then(data => setStockList(data))
+  }, [])
+
+  if( !stockList){
+    return <h2>Loading...</h2>
+  }
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList.map( stockItem => <Stock company={stockItem.name} price={stockItem.price} />)}
     </div>
   );
 }
